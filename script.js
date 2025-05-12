6ff6673cf08633f7570d1b2bdbbb05541ae06883
darkbehemoth13 <jefersonsaturnino13@gmail.com>
2025-05-11 21:22:10 -0400

    35 minutos de aula de 6 horas, além do conteúdo de JS em si, algumas estilizações simples adcionadas.

---
+++ script.js
@@ -0,0 +1,13 @@
+let numero1,numero2;
+numero1=1;
+numero2=10;
+
+
+console.log(numero1 + numero2);
+//Seleção do in com getElementById (atenção ao camel case) inner htMl
+document.getElementById("texto").innerHTML="Aulona top d++++ slc <b>JS</b>";
+
+console.log("Teste console");
+
+
+
