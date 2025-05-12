8432450be4ddb45edbabc4620d7f31adbbf530b4
darkbehemoth13 <jefersonsaturnino13@gmail.com>
2025-05-12 07:11:12 -0400

    1 hora e 20 minutos, conceitos sobre variáveis e introdução aos operadores.

--- script.js
+++ script.js
@@ -1,13 +1,63 @@
-let numero1,numero2;
-numero1=1;
-numero2=10;
+//Declaração de variáveis
+let a,b,c;
+//Atribuição de valores
+a=1;
+b=10;
+c=a+b;
 
 
-console.log(numero1 + numero2);
-//Seleção do in com getElementById (atenção ao camel case) inner htMl
+
+console.log(c);
+//Seleção do id com getElementById (atenção ao camel case) inner htMl
 document.getElementById("texto").innerHTML="Aulona top d++++ slc <b>JS</b>";
 
 console.log("Teste console");
+
+
+/*No JavaScript temos 3 tipos de variáveis, sendo elas:
+var---->  
+let---->
+const-->
+*/
+
+/*
+var pote = "bombom";
+alert(pote)
+
+
+let nome, sobrenome, nomeCompleto;
+//No caso de strings, o + faz concatenação
+nome = "Jeferson";
+sobrenome = "Saturnino";
+nomeCompleto = nome + " " + sobrenome;
+
+alert(nomeCompleto);
+*/
+
+//const batata = "batata";
+ 
+//batata = "batatinha"
+
+//retorna erro acima, tentando mudar valor de uma const
+
+//alert(batata);
+
+/* Operadores no JavaScript:
+
+São usados para atribuir valores, executar operações aritméticas e muito mais.
+
+sinais: + - * / = ++ -- -= && || etc...
+
+1) Operadores aritiméticos:
+*/
+
+let val1, val2, total;
+
+val1 = 3;
+val2 = 6;
+total = val1 + val2;
+
+alert(total);
 
 
 
